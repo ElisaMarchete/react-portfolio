1556964bbd5815f9ed0901f98cdd0f067530fd9e
ElisaMarchete <elisamarchete@gmail.com>
2023-07-20 11:19:00 -0400

    updated index.js from import ReactDOM from react-dom -> import { createRoot } from react-dom/client and render page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+
+// Use createRoot to create the root.
+const root = createRoot(rootElement);
+
+// Render to render the component tree.
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
